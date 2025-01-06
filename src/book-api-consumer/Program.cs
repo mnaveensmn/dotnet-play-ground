3b6129f7a394736cb592c0c32c79976f93cbd942
Naveen Kumar <naveen.kumar2@rxglobal.com>
2025-01-06 11:45:08 +0530

    commit local changes

---
+++ src/book-api-consumer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace book_api_consumer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var ApiClient = new ApiClient(new Uri("http://localhost:5091"));
+        var response = ApiClient.GetAllBooks().GetAwaiter().GetResult();
+        var responseBody = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+        Console.WriteLine($"Response.Code={response.StatusCode}, Response.Body={responseBody}\n\n");
+    }
+}
